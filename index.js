d20c4e5d54a8cb75d0aeb97ce29c8f8217328f37
arturshevchenko <shevchenkoarchi@gmail.com>
2023-03-10 21:55:30 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe","Ollie","Aki"];
+function writeCards(names, event){
+const messages =[];
+for (let i = 0; i < names.length; i++){
+messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+}
+return messages}
+console.log["Guadalupe","Ollie","Aki"]
+console.log(writeCards(names))
+debugger
+function countDown(){
+    let count = 5
+    while (--count > -7){
+        console.log(count)
+        debugger
+    }
+}
